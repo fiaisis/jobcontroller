f37f83381800f64373d3dd84a5e7843c89be398b
Keiran Price <keiran.price@stfc.ac.uk>
2025-06-20 13:46:44 +0100

    Move import

--- job_watcher/test/test_main.py
+++ job_watcher/test/test_main.py
@@ -3,6 +3,8 @@ import random
 import unittest
 from unittest import mock
 
+from jobwatcher.main import main
+
 
 class MainTest(unittest.TestCase):
     def setUp(self):
@@ -25,7 +27,7 @@ class MainTest(unittest.TestCase):
     @mock.patch("jobwatcher.main.JobWatcher")
     @mock.patch("jobwatcher.main.load_kubernetes_config")
     def test_main(self, load_kubernetes_config, job_watcher):
-        from jobwatcher.main import main
+
 
         main()
 
