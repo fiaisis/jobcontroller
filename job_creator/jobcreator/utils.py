d0913f620a5598e709dfcf4417cdfe5b8e4e5ee5
eva-1729 <eva.maitland@stfc.ac.uk>
2026-04-24 14:19:05 +0100

    add import

--- job_creator/jobcreator/utils.py
+++ job_creator/jobcreator/utils.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import requests
 from kubernetes import config  # type: ignore[import-untyped]
 from kubernetes.config import ConfigException  # type: ignore[import-untyped]
-from tenacity import retry, stop_after_attempt, wait_exponential
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
 stdout_handler = logging.StreamHandler(stream=sys.stdout)
 logging.basicConfig(
