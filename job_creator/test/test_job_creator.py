b68e1d31fac338d60489f62ed5f859a01c807ab7
eva-1729 <eva.maitland@stfc.ac.uk>
2026-09-25 08:49:15 +0100

    Try to adapt gem test since setup_manila_pv is generic now

--- job_creator/test/test_job_creator.py
+++ job_creator/test/test_job_creator.py
@@ -795,7 +795,8 @@ def test_jobcreator_spawn_job_dev_mode_true_gem(
         fs_name,
         ceph_mount_path,
     )
-    setup_manila_pv.assert_called_once_with(
+    assert setup_manila_pv.call_count == 2 # noqa: PLR2004
+    setup_manila_pv.assert_called_with(
         pv_name=f"{job_name}-ndxgem-pv",
         read_only=False,
         secret_namespace=job_namespace,
