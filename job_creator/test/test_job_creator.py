aef2a7a0cefb8de13c4b6f05fa7db615136dc975
eva-1729 <eva.maitland@stfc.ac.uk>
2026-09-25 10:57:03 +0100

    add any_order, put calls in list

--- job_creator/test/test_job_creator.py
+++ job_creator/test/test_job_creator.py
@@ -803,7 +803,7 @@ def test_jobcreator_spawn_job_dev_mode_true_gem(
         ceph_mount_path,
     )
     assert setup_manila_pv.call_count == 2  # noqa: PLR2004
-    manila_call_2 = call(
+    manila_call_1 = call(
         pv_name=f"{job_name}-extras-pv",
         read_only=True,
         secret_namespace=job_namespace,
@@ -811,7 +811,7 @@ def test_jobcreator_spawn_job_dev_mode_true_gem(
         manila_share_access_id=manila_share_access_id,
         access_mode="ReadOnlyMany",
     )
-    manila_call_1 = call(
+    manila_call_2 = call(
         pv_name=f"{job_name}-ndxgem-pv",
         read_only=False,
         secret_namespace=job_namespace,
@@ -819,8 +819,8 @@ def test_jobcreator_spawn_job_dev_mode_true_gem(
         manila_share_access_id=manila_share_access_id,
         access_mode="ReadWriteOnce",
     )
-    setup_manila_pv.assert_has_calls(manila_call_1, manila_call_2)
-    setup_manila_pv.assert_has_calls(manila_call_1, manila_call_2)
+    calls = [manila_call_1, manila_call_2]
+    setup_manila_pv.assert_has_calls(calls, any_order=True)
 
 
 @mock.patch("jobcreator.job_creator._setup_manila_pv")
