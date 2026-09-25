c8ffca99db1ce5b7fe06475f3fc05f87ad905605
eva-1729 <eva.maitland@stfc.ac.uk>
2026-09-25 11:00:11 +0100

    remove access_mode call for extras as it uses defaul

--- job_creator/test/test_job_creator.py
+++ job_creator/test/test_job_creator.py
@@ -809,7 +809,6 @@ def test_jobcreator_spawn_job_dev_mode_true_gem(
         secret_namespace=job_namespace,
         manila_share_id=manila_share_id,
         manila_share_access_id=manila_share_access_id,
-        access_mode="ReadOnlyMany",
     )
     manila_call_2 = call(
         pv_name=f"{job_name}-ndxgem-pv",
